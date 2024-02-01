0037c7608882295b7df08f0d5b7e136f4fbfb9a4
quanphu1902 <88502952+quanphu1902@users.noreply.github.com>
2024-02-01 11:00:04 +0700

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express'
-const app = express()
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
+const app = express()
+mongoose.connect(process.env.MONGO)
+.then(
+()=> {
+    console.log (' Connected')})
+.catch((err)=>{
+    console.log(err)
+})
 app.listen(3000,()=>{
     console.log('server is running 3000')
 })
