56adb23c810be4cc5944fcd56d0c71313cca0bde
quanphu1902 <88502952+quanphu1902@users.noreply.github.com>
2024-02-05 11:05:33 +0700

    middleware and function

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,29 @@
+import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js'
+
+export const signup = async(req,res,next)=>{
+   const {username , email , password} = req.body
+   if(!username|| !email || ! password || !username===''||email==='' || !password==='')
+   {
+    next(errorHandler(400,'all fields are requiered'))
+   }
+   const hashedPassword = bcryptjs.hashSync(password,10)
+   
+   const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+   })
+
+   try {
+    
+       await newUser.save()
+       res.json('Signup successful')
+   } catch (error) {
+    next(error)
+  
+    
+   }
+
+}
--- api/index.js
+++ api/index.js
@@ -1,16 +1,30 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js'
+import userRoutes from './routes/user.route.js'
 
 dotenv.config()
 const app = express()
+app.use(express.json())
 mongoose.connect(process.env.MONGO)
 .then(
 ()=> {
-    console.log (' Connected')})
+    console.log ('Connected')})
 .catch((err)=>{
     console.log(err)
 })
 app.listen(3000,()=>{
     console.log('server is running 3000')
 })
+ app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
