79a5b5a5e33fdf0d6595eb41d27c7e947150bc3d
quanphu1902 <88502952+quanphu1902@users.noreply.github.com>
2024-02-06 16:03:21 +0700

    Create sign in api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/DashBoard'
 import Projects from './pages/Projects'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -21,6 +22,7 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
 
       </Routes>
+      <Footer></Footer>
 
     </BrowserRouter>
   )
