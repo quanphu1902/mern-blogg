cbe4e6d5cd711d02f47d88c9e95fe19599ddc1fb
quanphu1902 <88502952+quanphu1902@users.noreply.github.com>
2024-03-03 13:31:16 +0700

    complete upload post image functionality

--- api/models/post.model.js
+++ api/models/post.model.js
@@ -1,40 +1,38 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-
-const postSchema = new mongoose.Schema({
-    userID:{
-        type: String,
-        require: true,
-
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
     },
     content: {
-        type: String,
-        require: true,
+      type: String,
+      required: true,
     },
-    title:{
-        type: String,
-        require: true,
-        unique: true,
-
+    title: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    image:{
-        type:String,
-        default: 'https://www.salesforce.com/ca/blog/wp-content/uploads/sites/12/2023/10/anatomy-of-a-blog-post-deconstructed-open-graph.jpg',
+    image: {
+      type: String,
+      default:
+        'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png',
     },
-    category:{
-        type : String,
-        default: 'uncategorized',
+    category: {
+      type: String,
+      default: 'uncategorized',
     },
     slug: {
-        type: String,
-        require: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
-    
-    
-},{timestamps:true}
-)
+  },
+  { timestamps: true }
+);
 
-const Post = mongoose.model('Post',postSchema);
+const Post = mongoose.model('Post', postSchema);
 
 export default Post;
