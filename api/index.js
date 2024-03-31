3f96c26047d6933bbe043f256998f268cf79e380
quanphu1902 <88502952+quanphu1902@users.noreply.github.com>
2024-03-31 15:09:51 +0700

    render

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRoutes from './routes/user.route.js'
 import commentRoutes from './routes/comment.route.js'
 import cookieParser from 'cookie-parser'
 import postRoutes from './routes/post.route.js'
+import path from 'path'
 
 dotenv.config()
 const app = express()
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGO)
 .catch((err)=>{
     console.log(err)
 })
+const __dirname = path.resolve()
 app.listen(3000,()=>{
     console.log('server is running 3000')
 })
@@ -25,6 +27,10 @@ app.listen(3000,()=>{
 app.use('/api/auth',authRoutes)
 app.use('/api/post',postRoutes)
 app.use('/api/comment',commentRoutes)
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
